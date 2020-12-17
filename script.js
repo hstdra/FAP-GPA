0189f371e113b7f7b55d0900b4080fcefad83ec6
Hồ Ngọc Vĩnh Hân <hstdra@gmail.com>
2020-12-18 01:26:57 +0700

    Convert jQuery to vanila JS

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const transcript = $("#ctl00_mainContent_divGrade table:first tbody tr")
-  .toArray()
-  .map((row) => ({
-    credit: +$(row).find("td")[7].innerText,
-    grade: +$(row).find("td")[8].innerText,
-  }))
-  .filter((row) => row.grade > 0 && row.credit > 0)
-  .reduce(
-  (acc, row) => {
-    acc.totalCredit += row.credit;
-    acc.totalScore += row.credit * row.grade;
-
-    return acc;
-  },
-  {
-    totalCredit: 0,
-    totalScore: 0,
-  }
+const transcriptRows = document.querySelectorAll(
+  "#ctl00_mainContent_divGrade > table > tbody > tr"
 );
+const transcript = [...transcriptRows]
+  .map((row) => ({
+    credit: +row.querySelectorAll("td")[7]?.innerText,
+    grade: +row.querySelectorAll("td")[8]?.innerText,
+  }))
+  .filter((row) => row && row.grade > 0 && row.credit > 0)
+  .reduce(
+    (acc, row) => {
+      acc.totalCredit += row.credit;
+      acc.totalScore += row.credit * row.grade;
+
+      return acc;
+    },
+    {
+      totalCredit: 0,
+      totalScore: 0,
+    }
+  );
 
 const currentGpa = (transcript.totalScore / transcript.totalCredit).toFixed(2);
 
 // Write GPA to page
-$("#ctl00_mainContent_lblRollNumber").append(` - <span class="label label-success">GPA: ${currentGpa}</span>`)
+document.getElementById(
+  "ctl00_mainContent_lblRollNumber"
+).innerHTML += ` - <span class="label label-success">GPA: ${currentGpa}</span>`;
